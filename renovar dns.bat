687c4353e8d383d99a18da7db0dae5d20ead4adc
nicolasgamer3 <140570496+nicolasgamer3@users.noreply.github.com>
2023-08-02 20:39:08 -0300

    Add files via upload

---
+++ renovar dns.bat
@@ -0,0 +1,26 @@
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
+REM Run shell as admin (
+color 0C
+:gotAdmin
+echo vamo spoofar essa merda 
+ipconfig /release *Con*
+echo spoofando .....
+ipconfig /flushdns
+echo spoofando .....
+ipconfig /release6
+echo spoofando .....
+ipconfig /registerdns
+echo spoofando .....
+ipconfig /displaydns
+
+
+echo spoofou! bom jogo Meu piru vai xitar fdp
+@echo 
+) - put here code as you like
+ECHO %batchName% Arguments: P1=%1 P2=%2 P3=%3 P4=%4 P5=%5 P6=%6 P7=%7 P8=%8 P9=%9
+cmd /k@echo off
+
+pause
